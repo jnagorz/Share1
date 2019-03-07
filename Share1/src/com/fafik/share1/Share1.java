4c92cbf8e7676553c97f0a8f2fafb61ba25837d1
jnagorz <47056393+jnagorz@users.noreply.github.com>
2019-03-07 18:51:11 +0100

    Update Share1.java
    Commit 7

--- Share1/src/com/fafik/share1/Share1.java
+++ Share1/src/com/fafik/share1/Share1.java
@@ -4,7 +4,10 @@ public class Share1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("fafik, klara, mysz, wróbelek");
+		System.out.println("Pies: fafik");
+		System.out.println("Kot: klara");
+		System.out.println("Ptak: Ä‡wirek");
+	
 
 	}
 
