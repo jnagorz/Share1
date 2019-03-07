315512c964b6bf71d662ae951b5b5dc5c1e5a304
Jacek <Jacek@Jacek-PC>
2019-03-07 18:45:50 +0100

    commit 6

--- Share1/src/com/fafik/share1/Share1.java
+++ Share1/src/com/fafik/share1/Share1.java
@@ -4,11 +4,7 @@ public class Share1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("fafik, klara, mysz, wróbelek");
-=======
-		System.out.println("fafik, klara, mysz");
->>>>>>> refs/remotes/origin/master
 
 	}
 
