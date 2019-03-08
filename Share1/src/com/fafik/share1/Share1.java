5314b5bba2a00edf07d8cc7c6bc05063578e7ced
Jacek <Jacek@Jacek-PC>
2019-03-08 20:00:55 +0100

    commit 12

--- Share1/src/com/fafik/share1/Share1.java
+++ Share1/src/com/fafik/share1/Share1.java
@@ -4,10 +4,10 @@ public class Share1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Dog: fafik");
-		System.out.println("Cat: klara");
-		System.out.println("Bird: cwirek");
-	
+		System.out.println("Pies: Fafik");
+		System.out.println("Kot: Klara");
+		System.out.println("Ptak: Cwirek");
+		System.out.println("Mysz: Miki");
 
 	}
 
