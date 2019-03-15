408e609427b722e589c76bddcb24f3c6692da201
Jacek1 <Jacek1@Jacek-PC>
2019-03-15 14:57:29 +0100

    Commit: 2019-03-15: godz:15:00

--- Share1/src/com/fafik/share1/Share1.java
+++ Share1/src/com/fafik/share1/Share1.java
@@ -6,5 +6,6 @@ public class Share1 {
 		// TODO Auto-generated method stub
 		System.out.println("Commit: 2019-03-13: godz:20:57");
 		System.out.println("Commit: 2019-03-14: godz:20:50");
+		System.out.println("Commit: 2019-03-15: godz:15:00");
 	}
 }
