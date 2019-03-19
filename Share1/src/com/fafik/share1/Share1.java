3fee7ec5dedee479a5ff5fd0bca8301ad0f1e1b1
Jacek <Jacek@Jacek-PC>
2019-03-19 22:35:45 +0100

    Commit: 2019-03-19: godz:22:35

--- Share1/src/com/fafik/share1/Share1.java
+++ Share1/src/com/fafik/share1/Share1.java
@@ -9,5 +9,6 @@ public class Share1 {
 		System.out.println("Commit: 2019-03-15: godz:15:00");
 		System.out.println("Commit: 2019-03-18: godz:21:46");
 		System.out.println("Commit: 2019-03-19: godz:13:48");
+		System.out.println("Commit: 2019-03-19: godz:22:35");
 	}
 }
